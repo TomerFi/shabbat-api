09a9d72642a9d01e8663feebde6f27bbe808aae3
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-08-01 16:56:17 +0300

    build: added license headers to shell scripts

--- scripts/get_next_bump.sh
+++ scripts/get_next_bump.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Copyright Tomer Figenblat.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#limitations under the License.
+
 show_usage() {
 	echo "Script returing the next recommended semantic version based on git tags and commits"
 	echo "-----------------------------------------------------------------------------------"
@@ -8,6 +20,7 @@ show_usage() {
 	echo "Example output: 2.1.17"
 	echo ""
 	echo "** requires nodejs >= 14.0.0"
+	echo "** requires 'npm i -g conventional-recommended-bump git-semver-tags'"
 }
 # show usage if asked for help
 if [[ ($# == "--help") || $# == "-h" ]]; then
@@ -17,10 +30,10 @@ fi
 # utility function for incrementing the bump number
 increment() { echo $(("$1" + 1)); }
 # use git-semver-tags to get the latest semver tag and read its parts seperately
-last_semver=$(npx git-semver-tags | head -n 1)
+last_semver=$(git-semver-tags | head -n 1)
 read last_major last_minor last_patch <<<$(sed "s/\./ /g" <<<$last_semver)
 # use conventional-recommended-bump to get the next bump recommendation
-rec_bump=$(npx conventional-recommended-bump -p conventionalcommits -t "")
+rec_bump=$(conventional-recommended-bump -p conventionalcommits -t "")
 # create the next semver release based on the bump recomendation
 if [ $rec_bump = "major" ]; then
 	# bump major
--- scripts/get_next_dev_iteration.sh
+++ scripts/get_next_dev_iteration.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Copyright Tomer Figenblat.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#limitations under the License.
+
 show_usage() {
 	echo "Script returing the next java development iteration for a semantic version"
 	echo "--------------------------------------------------------------------------"
@@ -7,16 +19,10 @@ show_usage() {
 	echo ""
 	echo "Example: $0 --tag 2.1.17"
 	echo "Will output: 2.1.18-SNAPSHOT"
-	echo ""
-	echo "** requires nodejs >= 14.0.0"
 }
 if [[ ($# == "--help") || $# == "-h" ]]; then
 	show_usage
 	exit 0
-fi
-if [ -z "$1" ]; then
-	show_usage
-	exit 1
 fi
 # iterate over arguments and create named parameters
 while [ $# -gt 0 ]; do
